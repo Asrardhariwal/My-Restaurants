dd02b9e0877bdc56101869b89a29a3897e986c09
Asrardhariwal <asrardhariwal1@gmail.com>
2021-09-17 22:50:22 +0500

    third commit

--- src/components/menuApi.js
+++ src/components/menuApi.js
@@ -81,15 +81,16 @@ const Menu = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
-  // {
-  //   id: 9,
-  //   image: "../images/samosa.jpg",
-  //   name: "samaso",
-  //   category: "newDish",
-  //   price: "10₹",
-  //   description:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-  // },
+  // New dish added
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "newDish",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
 ];
 
 export default Menu;
